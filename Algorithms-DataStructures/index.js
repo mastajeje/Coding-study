64f5da1fc2b2418f8b2d37d1f8d674912d65fd36
flexing1010 <kjzon@hotmail.com>
2021-10-20 18:59:42 +0900

    Study Sort Algorithms(insertion sort) Implemented sort algorithm

--- Algorithms-DataStructures/index.js
+++ Algorithms-DataStructures/index.js
@@ -1,5 +1,5 @@
 const numbers = [5, 3, 2, 4, 1, 7, 9, 8, 6];
-// const numbers = [3, 2, 1];
+// const numbers = [2, 1, 4, 3];
 
 // Bubble Sort O(n^2)
 const bubbleSort = (arr) => {
@@ -35,6 +35,25 @@ const selectionSort = (arr) => {
   }
 };
 
+// Insertion Sort
+const insertionSort = (arr) => {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    if (arr[i] < arr[0]) {
+      arr.unshift(arr.splice(i, 1)[0]);
+    } else {
+      if (arr[i] < arr[i - 1]) {
+        for (let j = 1; j < i; j++) {
+          if (arr[i] > arr[j - 1] && arr[i] < arr[j]) {
+            arr.splice(j, 0, arr.splice(i, 1)[0]);
+          }
+        }
+      }
+    }
+  }
+};
+
 // bubbleSort(numbers);
-selectionSort(numbers);
+// selectionSort(numbers);
+insertionSort(numbers);
 console.log(numbers);
